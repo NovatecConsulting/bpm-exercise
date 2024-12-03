16f2aff3a55ee2e9b0d92e81a0d65f2eb1df4885
Lukas Spillner <spillnerlu+github@gmail.com>
2024-12-03 13:13:06 +0100

    Switched to Camunda 8 spring starter SDK

--- chapter3-camunda8/exam-registration-c8/src/main/java/de/novatec/bpm/Application.java
+++ chapter3-camunda8/exam-registration-c8/src/main/java/de/novatec/bpm/Application.java
@@ -1,19 +1,15 @@
 package de.novatec.bpm;
 
-import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import org.camunda.community.migration.adapter.EnableCamunda7Adapter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
+
 @SpringBootApplication
-@EnableZeebeClient
 @EnableCamunda7Adapter
+@Deployment( resources = { "classpath:/process/converted-c8-exam-registration.bpmn" , "classpath:/process/examRegistrationPeriod.dmn"})
 public class Application {
-
-    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
-
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
     }
